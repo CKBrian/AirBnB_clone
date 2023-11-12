cb63c0b935c49e97be3e60bf19b2f88223d1c760
Brian <chumobrian@gmail.com>
2023-11-12 23:45:55 +0300

    feat: Add a script that contains unittest test cases for testing BaseModel class

--- tests/test_base_model.py
+++ tests/test_base_model.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 """defines test_base_model modules with testcases for base_model module"""
-import unittest
 from unittest import TestCase
 from models.base_model import BaseModel
+from models.engine.file_storage import FileStorage
+from io import StringIO
+import unittest
+import sys
 
 
 class TestBaseModel(TestCase):
@@ -17,13 +20,39 @@ class TestBaseModel(TestCase):
         mod3 = BaseModel(**dict_obj)
         self.assertEqual(mod3.to_dict(), dict_obj)
 
-    def test___str__(self):
-        """tests for __str__ method"""
-        pass
-
     def test_save(self):
         """tests for save method"""
-        pass
+        model1 = BaseModel()
+        obj_id1 = "BaseModel.{}".format(model1.id)
+        model1.save()
+        obj_saved = model1.to_dict()
+        storage = FileStorage()
+        storage.reload()
+        model2 = BaseModel(**obj_saved)
+        obj_reloaded = model2.to_dict()
+        self.assertEqual(obj_saved, obj_reloaded)
+
+    def test__str__(self):
+        """tests for __str__ method"""
+        output = StringIO()
+        sys.stdout = output
+
+        m1 = BaseModel()
+        print(m1)
+        dict_obj = m1.to_dict()
+
+        sys.stdout = sys.__stdout__
+        res1 = output.getvalue()
+
+        output = StringIO()
+        sys.stdout = output
+
+        m2 = BaseModel(**dict_obj)
+        print(m2)
+
+        sys.stdout = sys.__stdout__
+        res2 = output.getvalue()
+        self.assertEqual(res1, res2)
 
     def test_to_dict(self):
         """tests for to_dict method"""
